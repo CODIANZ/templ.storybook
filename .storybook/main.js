c802bbf468b5be8b9637dcfd9d1d87f2c3f22653
terukazu inoue <terukazu.inoue@codianz.com>
2021-02-10 15:32:01 +0900

    storybookでsass/scssに対応、取り敢えずpugも設定しといた

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,6 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     {
       name: "@storybook/addon-essentials",
@@ -12,5 +9,18 @@ module.exports = {
       }
     },
     "@storybook/addon-knobs"
-  ]
-}
+  ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.s[ac]ss$/i,
+      use: ["style-loader", "css-loader", "sass-loader"]
+    });
+    /* もしかすると pug 使うかもなので、取り敢えず定義だけしとく */
+    /* pug 使うなら pug-plain-loader をインストール */
+    config.module.rules.push({
+      test: /\.pug$/,
+      use: [{ loader: "pug-plain-loader" }]
+    });
+    return config;
+  }
+};
